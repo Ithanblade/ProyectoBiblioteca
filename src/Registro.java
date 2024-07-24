18e3b32d70c710734f67d21e473a4c8231ecbddf
IthanC <ithan.camacho@epn.edu.ec>
2024-07-23 20:04:11 -0500

    Pruebas con la coneccion a Atlas, al parecer da el mismo error que en clase, este se soluciono dias despues al quitar y poner la libreria, se esperará para hacer lo mismo en este caso. Coneccion pendiente...

--- src/Registro.java
+++ src/Registro.java
@@ -1,8 +1,10 @@
-import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
 import org.bson.Document;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.result.UpdateResult;
 
 import javax.swing.*;
 import java.awt.*;
